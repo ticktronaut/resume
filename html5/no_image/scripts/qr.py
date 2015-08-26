142c7c4d58c65a8cfa3e47e1c6b892ed9ae7771b
nerdOmat <ghandi_84@hotmail.com>
2015-08-27 00:44:55 +0200

    Improved print.css, added qr-code (and python-script>)

--- html5/no_image/scripts/qr.py
+++ html5/no_image/scripts/qr.py
@@ -6,4 +6,8 @@ import pyqrcode
 print "Create qr-code."
 # create qrcodes
 url = pyqrcode.create("github.com/nerdOmat/resume")
-url.svg('../images/qr.svg', scale=10)
+url.svg('../images/qr.svg', scale=2.25)
+
+print "Convert qr-code from svg to png."
+os.system("inkscape --without-gui --export-dpi=150 --file=../images/qr.svg --export-png=../images/qr.png")
+#os.system("mogrify -resize 55% ../images/qr.png")
